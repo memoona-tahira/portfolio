fe657bee25b57a5f613dde433af8720b991cd22e
memoona.tahira <memoona.tahira@gmail.com>
2025-10-20 12:22:48 +0200

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,18 @@
+import React from "react";
 
-  function Header(){
-    return(
-        <div>
+function Header() {
+  return (
+    <header className="header">
+      <h2 className="logo">Memoona</h2>
+      <nav className="nav">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
 
-        </div>
-    );
-  }
-
-
-  export default Header;
+export default Header;
